0aa235faa78c9b985e70a768cc07434fd23c1ecb
OutcastsRobotics <communityroboticsboosters@gmail.com>
2025-09-27 19:13:45 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+@TeleOp
 public class motorTest extends OpMode {
 
     DcMotor motor;
