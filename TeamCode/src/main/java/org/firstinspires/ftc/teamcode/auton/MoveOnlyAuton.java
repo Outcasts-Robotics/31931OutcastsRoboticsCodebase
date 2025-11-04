93006e4819435f8c43881d2d3d216696831c728d
chujtan <chujtan@gmail.com>
2025-11-04 17:24:35 -0600

    Add a simple move only auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/MoveOnlyAuton.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous
+public class MoveOnlyAuton extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        final DcMotor frontLeft = hardwareMap.get(DcMotor.class, "fl");
+        final DcMotor frontRight = hardwareMap.get(DcMotor.class, "fr");
+        final DcMotor rearRight = hardwareMap.get(DcMotor.class, "rr");
+        final DcMotor rearLeft = hardwareMap.get(DcMotor.class, "rl");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeft.setPower(-.3);
+        frontRight.setPower(.3);
+        rearLeft.setPower(.3);
+        rearRight.setPower(-.3);
+
+        Thread.sleep(1000);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        rearLeft.setPower(0);
+        rearRight.setPower(0);
+    }
+}
