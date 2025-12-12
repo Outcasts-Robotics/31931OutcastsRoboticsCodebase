192e35ce0f897fd5b451dcee307c8f0b1dc71519
ProtoTypd1 <167651856+ProtoTypd1@users.noreply.github.com>
2025-12-12 16:43:48 -0600

    fuck me rahh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/MecanumDrive.java
@@ -32,7 +32,7 @@ public class MecanumDrive {
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        maxPower = 0.8;
+        maxPower = 1.0;
         this.yawInRadProvider = yawInRadProvider;
     }
 
