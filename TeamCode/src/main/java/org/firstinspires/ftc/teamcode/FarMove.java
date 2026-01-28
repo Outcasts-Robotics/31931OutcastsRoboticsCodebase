3eed113b3903db858334c17100b4e8339aaa97e7
Clayton Jin <Clayton.jin@outlook.com>
2026-01-27 19:44:51 -0600

    blahhhh changed one thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FarMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FarMove.java
@@ -32,7 +32,7 @@ public class FarMove extends LinearOpMode {
         rearLeft.setPower(.3);
         rearRight.setPower(.3);
 
-        Thread.sleep(500);
+        Thread.sleep(450);
 
         frontLeft.setPower(0);
         frontRight.setPower(0);
