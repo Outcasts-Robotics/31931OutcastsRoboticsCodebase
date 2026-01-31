cd5ae4122bb5f1705c5882aedcd6f06d357f4a38
Clayton Jin <Clayton.jin@outlook.com>
2026-01-30 19:32:39 -0600

    blahh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tunings/Hopeful.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.annotation.SuppressLint;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.components.Launcher;
+
+@Autonomous
+public class Hopeful extends LinearOpMode {
+    @SuppressLint("NewApi")
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        final DcMotor frontLeft = hardwareMap.get(DcMotor.class, "fl");
+        final DcMotor frontRight = hardwareMap.get(DcMotor.class, "fr");
+        final DcMotor rearRight = hardwareMap.get(DcMotor.class, "rr");
+        final DcMotor rearLeft = hardwareMap.get(DcMotor.class, "rl");
+        final Launcher launcher = new Launcher(hardwareMap, gamepad1);
+
+        launcher.init();
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeft.setPower(1);
+        frontRight.setPower(1);
+        rearLeft.setPower(1);
+        rearRight.setPower(1);
+
+        Thread.sleep(4000);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        rearLeft.setPower(0);
+        rearRight.setPower(0);
+        telemetry.addLine("Turning...");
+        telemetry.update();
+
+        frontRight.setPower(1);
+        rearLeft.setPower(1);
+        Thread.sleep(200);
+        frontRight.setPower(0);
+        rearLeft.setPower(0);
+
+        telemetry.addLine("Launching...");
+        telemetry.update();
+
+
+        launcher.launch();
+        Thread.sleep(300);
+
+
+        telemetry.addLine("Stopping launcher");
+        telemetry.update();
+        launcher.onStop();
+        telemetry.addLine("Stopped launcher");
+        telemetry.update();
+
+
+
+    }
+}
+//blahh
